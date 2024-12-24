7c697a9d0a0b53d6296ec3bfd6d9aed14198cc77
MrEdgarr <nqhuy11101@gmail.com>
2024-12-24 20:12:41 +0700

    seats

--- controller/seat.controller.js
+++ controller/seat.controller.js
@@ -4,7 +4,9 @@ const seatController = {
         try {
             const { room_id } = req.body;
             const [rows, fields] = await pool.query(
-                `SELECT tb1.seat_row, ( JSON_ARRAYAGG( JSON_OBJECT( 'id', tb1.seatId, 'seat_type', tb1.seat_type, 'room_id', tb1.roomId, 'seat_number', tb1.seat_number, 'seat_status', tb1.seat_status ) ) ) seatResult from ( SELECT seats.id AS seatId, seats.seat_type,seats.seat_row,seats.seat_number,seats.seat_status, rooms.id AS roomId FROM seats INNER JOIN rooms ON rooms.id = seats.room_id WHERE rooms.id = ? ) tb1 GROUP BY tb1.seat_row, tb1.roomId ORDER BY tb1.seat_number ASC`,
+                `
+                SELECT * FROM seats WHERE seats.room_id = ?;
+               `,
                 [room_id]
             );
 
